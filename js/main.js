b9ef0c538b826844e4c3be8d003d5883ab923a6c
bhumesh2001 <bhumesh21@navgurukul.org>
2024-10-15 17:58:01 +0530

    new code

--- js/main.js
+++ js/main.js
@@ -153,18 +153,18 @@
   }
   smoothScroll();
 
-  var dateAndTime = function() {
-    $('#m_date').datepicker({
-      'format': 'm/d/yyyy',
-      'autoclose': true
-    });
-    $('#checkin_date, #checkout_date').datepicker({
-      'format': 'd MM, yyyy',
-      'autoclose': true
-    });
-    $('#m_time').timepicker();
-  };
-  dateAndTime();
+  // var dateAndTime = function() {
+  //   $('#m_date').datepicker({
+  //     'format': 'm/d/yyyy',
+  //     'autoclose': true
+  //   });
+  //   $('#checkin_date, #checkout_date').datepicker({
+  //     'format': 'd MM, yyyy',
+  //     'autoclose': true
+  //   });
+  //   $('#m_time').timepicker();
+  // };
+  // dateAndTime();
 
 
   var windowScroll = function() {
